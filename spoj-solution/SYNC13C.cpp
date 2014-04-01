827eab6d07fe4ee110dd2f5fcdec74ff832b79fa
rawain <rawain@users.noreply.github.com>
2014-04-01 14:55:07 +0530

    Create SYNC13C.cpp

---
+++ spoj-solution/SYNC13C.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    { int a,b;
+     cin>>a>>b;
+     if(a%2!=0&&b%2!=0)
+     { cout<<"Ramesh"<<endl;
+     }
+     else
+     cout<<"Suresh"<<endl;
+     }
+     system("pause");
+     return 0;
+     }
+
