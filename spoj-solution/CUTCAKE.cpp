38e84cdf382b65a616a682b80c2a436bc12ffff1
rawain <rawain@users.noreply.github.com>
2014-04-01 14:55:52 +0530

    Create CUTCAKE.cpp

---
+++ spoj-solution/CUTCAKE.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int t;
+    unsigned long long  n;
+  cin>>t;
+    while(t--)
+    {
+            cin>>n;
+            cout<<(sqrt(8*n-7)-1)/2<<endl;
+            }
+            system("pause");
+            return 0;
+            }
+
+
+
+
+
